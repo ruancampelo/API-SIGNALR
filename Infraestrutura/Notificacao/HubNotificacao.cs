bb83a3688d1ffef5b626c217554b2b959783a6b6
Ruan Campelo Neri <ruan.campelo@g.globo>
2024-03-01 14:15:40 -0300

    .

--- Infraestrutura/Notificacao/HubNotificacao.cs
+++ Infraestrutura/Notificacao/HubNotificacao.cs
@@ -3,16 +3,5 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Infraestrutura.Notificacao
 {
-    public class HubNotificacao : Hub 
-    {
-        public async Task EnviarNotificacao(string message)
-        {
-            await Clients.All.SendAsync(message);
-        }
-
-        public async Task EnviarNotificacaoPorConnectionId(string userId, string message)
-        {
-            await Clients.User(userId).SendAsync("EnviarNotificacaoByUserId", message);
-        }
-    }
+    public class HubNotificacao : Hub { }
 }
